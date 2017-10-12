332005f4ecaee59ac76f3aaec8e7b74757245509
Vagner Santana <vagnervst17@gmail.com>
2017-10-12 19:33:19 -0300

    add config file

---
+++ src/config.js
@@ -0,0 +1,18 @@
+import convict from 'convict';
+
+const config = convict({
+  github: {
+    doc: "Github V4 API",
+    url: "https://api.github.com/graphql",
+    token: {
+      doc: 'The github API token.',
+      format: String,
+      env: 'GITHUB_API_TOKEN',
+    }
+  },
+  mongo: {
+    host: 'mongodb://localhost/perryworker'
+  }
+});
+
+export default config;
