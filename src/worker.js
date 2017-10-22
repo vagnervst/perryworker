50ba6b5b011d80754a34ad5f06472d324d623812
Vagner Santana <vagnervst17@gmail.com>
2017-10-21 22:13:01 -0200

    worker: update worker to use promises

--- src/worker.js
+++ src/worker.js
@@ -19,18 +19,24 @@ function Worker(requests) {
 
   return {
     run: function() {
+      console.log(chalk.yellow('Worker started executing...'));
 
-      Promise.all(getPromises())
+      return Promise.all(getPromises())
       .then( responses => {
 
-        let processedResponses = [];
+        let registratorPromises = [];
         for( let i = 0; i < responses.length; ++i ) {
-          requests[i].registrator( responses[i] );
+          let registratorPromise = requests[i].registrator( responses[i] );
+          registratorPromises.push( registratorPromise );
         }
+
+        return Promise
+        .all(registratorPromises)
+        .then( () => console.log(chalk.yellow('Worker finished execution')));
 
       })
       .catch( error => {
-        console.log( error )
+        console.log( error );
       });
 
     }
