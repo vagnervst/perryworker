cd6b48ddf3b5fd24c5d8622bfbf6b8e952231e6e
Vagner S <vagnervst17@gmail.com>
2017-10-31 20:16:43 -0200

    create sieve module
    Create module for filtering array values

--- src/filter.js
+++ src/filter.js
@@ -1,16 +1,69 @@
-function BlackList(toFilterItems) {
-  return {
-    has: item => {
+/**
+  @param {Object} filterOptions
+  @param {Array} filterOptions.blackList
+  @param {Array} filterOptions.whiteList
+  @param {Array} filterOptions.itemsToFilter
+  @param {Function} getValueToCompare
+*/
+function Sieve(filterOptions, getValueToCompare) {
 
-      for( let i = 0; i < toFilterItems.length; i += 1 ) {
-        if( item === toFilterItems[i] ) {
-          return true;
-        }
+  function filterUsingWhiteList() {
+    return filterInclusive( filterOptions.whiteList );
+  }
+
+  function filterUsingBlackList() {
+    return filterExclusive( filterOptions.blackList );
+  }
+
+  function filterInclusive( comparableList ) {
+    let filteredItems = [];
+
+    filterOptions.itemsToFilter.forEach( item => {
+      if( has(comparableList, getValueToCompare(item)) ) {
+        filteredItems.push(item);
+      }
+    });
+
+    return filteredItems;
+  }
+
+  function filterExclusive( comparableList ) {
+    let filteredItems = [];
+
+    filterOptions.itemsToFilter.forEach( item => {
+      if( !has(comparableList, getValueToCompare(item)) ) {
+        filteredItems.push(item);
+      }
+    });
+
+    return filteredItems;
+  }
+
+  function has( comparableList, value ) {
+
+    for( let i = 0; i < comparableList.length; i += 1 ) {
+      if( comparableList[i] === value ) return true;
+    }
+
+    return false;
+  }
+
+  return {
+    mitigate: function() {
+      if( filterOptions.blackList && filterOptions.whiteList ) {
+        throw {
+          type: 'FilterException',
+          message: "Can't filter for both blacklist and whitelist"
+        };
       }
 
-      return false;
+      if( filterOptions.blackList ) {
+        return filterUsingBlackList();
+      } else if( filterOptions.whiteList ) {
+        return filterUsingWhiteList();
+      }
     }
   }
 }
 
-export default BlackList;
+export default Sieve;
